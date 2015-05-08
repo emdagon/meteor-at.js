0e3155f772a553b200478866b03156c7259d8ead
Emilio Daniel González <emdagon@gmail.com>
2015-05-08 12:35:10 -0400

    +Meteor package.js, +At.js as submodule

---
+++ package.js
@@ -0,0 +1,13 @@
+Package.describe({
+  name: "emdagon:at.js",
+  summary: "Meteor package for At.js",
+  version: "0.0.1",
+  git: "https://github.com/emdagon/meteor-at.js"
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom("1.0");
+  api.use("jquery", "client")
+  api.use("jameslefrere:caretjs@0.0.1", "client")
+  api.addFiles(["At.js/dist/css/jquery.atwho.css", "At.js/dist/js/jquery.atwho.js"], "client");
+});
